61ddc168610cf38b0d29da2a334beacd3ce0ffe8
MohammedALi <zringa98@gmail.com>
2023-12-05 11:50:02 +0100

    Initial commit

---
+++ app/src/main/java/com/example/catchat/InboxFragment.kt
@@ -0,0 +1,17 @@
+package com.example.catchat
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class InboxFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_inbox, container, false)
+    }
+}
